8dbc952991a4366d7019eaaf45fe6c8476969647
chunxia zhang <persephonezcx@gmail.com>
2015-09-28 23:03:07 -0400

    add popup dialog

---
+++ log4420_app/public/js/client_game.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Zoe on 15-09-27.
+ */
+
+
+$(document).ready(function(){
+    $('#special_items_btn').click(function(){
+        $('#special_items').toggle();
+    });
+});
+
+$(document).ready(function(){
+    $('#backpack_items_btn').click(function(){
+        $('#backpack_items').toggle();
+    });
+});
+
